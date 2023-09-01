e0988934d7aec3ef4fe2a48db5d69cce3cba12db
Paula Guzman <pguzman@possibilitycompany.com>
2023-09-01 15:09:08 -0500

    Added cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application } from 'express'
 import morgan from 'morgan'
+import cors from 'cors';
 
 //Routes
 import IndexRoutes from './routes/index.routes'
@@ -13,32 +14,39 @@ import DependienteRoutes from "./routes/dependiente.routes";
 export class App {
     private app: Application
 
-    constructor(private port?: number | string){
+    constructor(private port?: number | string) {
         this.app = express()
         this.settings()
         this.middlewares()
         this.routes()
+        this.use()
     }
 
-    settings(){
+    settings() {
         this.app.set('port', this.port || process.env.PORT || 3000)
     }
 
-    middlewares(){
+    middlewares() {
         this.app.use(morgan('dev'))
         this.app.use(express.json())
     }
 
-    routes(){
+    routes() {
         this.app.use(IndexRoutes)
-        this.app.use('/municipios',MunicipioRoutes)
+        this.app.use('/municipios', MunicipioRoutes)
         this.app.use('/personas', PersonaRoutes)
         this.app.use('/viviendas', ViviendaRoutes)
         this.app.use('/posee', PoseeRoutes)
         this.app.use('/dependiente', DependienteRoutes)
     }
 
-    async listen(){
+    use() {
+        this.app.use(cors);
+        this.app.use(express.json());
+        console.log('Cors available!')
+    }
+
+    async listen() {
         await this.app.listen(this.app.get('port'))
         console.log('Sever on port', this.app.get('port'))
     }
